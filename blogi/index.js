b0ca3180469e86eee54b4fd868a1873a4fdc5fc7
Minttu Paalanen <minttupaalanen@gmail.com>
2025-08-03 22:28:35 +0300

    4.1-4.2

---
+++ blogi/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const Blog = require("./models/blogs")
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
