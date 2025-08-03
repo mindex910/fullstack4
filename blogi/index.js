bd5ef206e179803e78c656ea0870ed3575c207ca
Minttu Paalanen <minttupaalanen@gmail.com>
2025-08-03 23:44:14 +0300

    4.13-4.14

--- blogi/index.js
+++ blogi/index.js
@@ -1,23 +1,34 @@
-const express = require('express')
-const app = express()
-const Blog = require("./models/blogs")
+const express = require("express");
+const app = express();
+const Blog = require("./models/blogs");
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
+app.get("/api/blogs", (request, response) => {
   Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", async (request, response) => {
+  const blog = new Blog(request.body);
+  const result = await blog.save();
+  response.status(201).json(result);
+});
+
+app.delete("/api/blogs/:id", async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+app.put("/api/blogs/:id", async (request, response) => {
+  const blog = request.body
+  const payload = {likes: blog.likes}
+  await Blog.findByIdAndUpdate(request.params.id, payload, {new: true})
+  response.status(200).end()
 })
 
-const PORT = 3003
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
